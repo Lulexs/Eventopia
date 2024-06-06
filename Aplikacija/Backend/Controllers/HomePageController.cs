6f24d6aa461ada9bd8fde95dc7ed51ae69242c54
veljkozivkovic <vzvzivkovic7@gmail.com>
2024-06-06 20:32:08 +0200

    HomePageUtils(mostly for recommendation, FullEventForRecomm for GetRecommendedEvents, GetRecommendedEvents

--- Aplikacija/Backend/Controllers/HomePageController.cs
+++ Aplikacija/Backend/Controllers/HomePageController.cs
@@ -1,5 +1,10 @@
 namespace Backend.Controllers;
 
+
+
+
+
+
 [Authorize]
 [ApiController]
 [Route("[controller]")]
@@ -161,6 +166,120 @@ public class HomePageController : ControllerBase
 
         return Ok(tags);
     }
+
+
+
+
+
+    [AllowAnonymous]
+    [HttpGet("GetRecommendedEvents")]
+    public async Task<IActionResult> GetRecommendedEvents(FullEventDto trenutniDogadjaj)
+    {
+        List<Dogadjaj> dogadjaji = await Context.Dogadjaji
+                                    .Include(x => x.Organizator)
+                                    .Include(x => x.Tagovi)
+                                    .Include(x => x.RezervacijaProstora)
+                                    .Where(x => x.Vreme > DateTime.Now && x.Status == StatusDogadjaja.Active)
+                                    .ToListAsync();
+
+        var trenutni = await Context.Dogadjaji
+                                    .Include(x => x.Organizator)
+                                    .Include(x => x.Tagovi)
+                                    .Include(x => x.RezervacijaProstora)
+                                    .FirstOrDefaultAsync(x => x.ID == trenutniDogadjaj.ID);
+
+        if (trenutni == null)
+            return BadRequest("Event does not exist.");
+
+        List<FullEventDto> povratniDogadjaji = new List<FullEventDto>();
+
+        List<FullEventForRecomm> zaRejtovanje = new List<FullEventForRecomm>();
+        
+        double trenutniLat = trenutni.RezervacijaProstora!.Prostor!.Latitude;
+        double trenutniLongt = trenutni.RezervacijaProstora.Prostor.Longitude;
+
+        foreach (var dogadjaj in dogadjaji)
+        {
+            if (dogadjaj.ID == trenutni.ID)
+                continue;
+
+            double rejt = 0;
+
+
+            //pogadjanja u tagovima
+            if (trenutni.Tagovi != null && dogadjaj.Tagovi != null)
+            {
+                foreach (var tag in trenutni.Tagovi)
+                {
+                    foreach(var tag2 in dogadjaj.Tagovi)
+                    {
+                        if (HomePageUtils.LevenshteinDistance(tag.TagName, tag2.TagName) < 3)
+                            rejt += 1;
+                    }
+                 }
+            }
+
+            //mora vidim da l da ogranicim rejt za pogadjanje u tagovima
+            rejt = rejt > 10 ? 10 : rejt;
+
+            //max je 10 za udaljenost
+            //pogadjanje u lokaciji
+            double dogadjajLat = dogadjaj.RezervacijaProstora!.Prostor!.Latitude;
+            double dogadjajLongt = dogadjaj.RezervacijaProstora.Prostor.Longitude;
+            double distance = HomePageUtils.HaversineDistance(trenutniLat, trenutniLongt, dogadjajLat, dogadjajLongt)/ 1000; // da bude u km
+            rejt += HomePageUtils.CalculateScoreDistance(distance, 300); // znaci kad dalje od 300km, rejt je 0
+
+
+            //max je 10 za vreme
+            //pogadjanje u vremenu
+            double vreme = Math.Abs((trenutni.Vreme - dogadjaj.Vreme).TotalHours);
+            rejt += HomePageUtils.CalculateScoreTime(vreme, 72); // znaci kad je vise od 74h razlike, rejt opada linearno
+
+            
+            //max je 10 za rezervacije
+            //izvlacenje ocene na osnovu broja rezervacija
+            int rezervisanaMesta = dogadjaj.Rezervacije!.Sum(rezervacija => rezervacija.BrojMesta);
+            rejt += HomePageUtils.CalculateScoreReservation(rezervisanaMesta, 100); // znaci kad je vise od 100 mesta rezervisano, rejt opada linearno
+
+            zaRejtovanje.Add(new FullEventForRecomm
+            {
+                ID = dogadjaj.ID,
+                Naziv = dogadjaj.Naziv,
+                Slika = dogadjaj.Slika,
+                Datum = dogadjaj.Vreme.ToString("dd.MM.yyyy."),
+                Vreme = dogadjaj.Vreme.ToString("HH:mm"),
+                Lokacija = $"{dogadjaj.RezervacijaProstora!.Prostor!.Grad}, {dogadjaj.RezervacijaProstora.Prostor.Drzava}",
+                OrganizatorID = dogadjaj.Organizator!.Id.ToString(),
+                Organizator = $"{dogadjaj.Organizator!.Ime} {dogadjaj.Organizator!.Prezime}",
+                Rating = rejt
+            });
+
+        }
+
+        zaRejtovanje = zaRejtovanje.OrderByDescending(x => x.Rating).ToList();
+
+        foreach (var dogadjaj in zaRejtovanje)
+        {
+            povratniDogadjaji.Add(new FullEventDto
+            {
+                ID = dogadjaj.ID,
+                Naziv = dogadjaj.Naziv,
+                Slika = dogadjaj.Slika,
+                Datum = dogadjaj.Datum,
+                Vreme = dogadjaj.Vreme,
+                Lokacija = dogadjaj.Lokacija,
+                OrganizatorID = dogadjaj.OrganizatorID,
+                Organizator = dogadjaj.Organizator
+            });
+        }
+
+
+        return Ok(povratniDogadjaji);
+
+    }
+
+
+
 
 
 }
